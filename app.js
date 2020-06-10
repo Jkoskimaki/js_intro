82a4a57cf3bbd8f58e709a9a3a1c78991f4f6683
Jacob Koskimaki <jkoskimaki@gmail.com>
2020-06-09 20:26:25 -0600

    tacos

---
+++ app.js
@@ -0,0 +1,17 @@
+document.getElementById("demo").innerHTML = "Hello World"
+// getElementsByTagName("p")
+// getElementsByClassName("header")
+
+document.getElementById("taco").style.width = '300px'
+
+// Can change source of an image with JS
+
+// event listeners ... JS has a way for you to target individual events
+document.getElementById('btn').addEventListener("click", changePic)
+
+function changePic() {
+ if (taco.src  )
+}
+
+
+
